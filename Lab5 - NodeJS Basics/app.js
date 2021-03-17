e27eec70b60a68f949b31e9a4cea4f8914c3c7c3
Bob Storms <bob.storms@hotmail.be>
2021-03-17 11:57:00 +0100

    Setup file structure of routers and controllers

--- Lab5 - NodeJS Basics/app.js
+++ Lab5 - NodeJS Basics/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const routerMessages = require("./routers/messages");
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use("/api/messages", routerMessages);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ Lab5 - NodeJS Basics/routers/messages.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const controllerMessages = require("../controllers/messages");
+
+// API Endpoints
+
+module.exports = router;
