953ba501d77ea7fdb94f8b533eb6d43ba5e01ff8
Bob Storms <bob.storms@hotmail.be>
2021-03-24 20:28:02 +0100

    feat: add view and route for updating stats

---
+++ Lab6 - Web Sockets/routes/updatestats.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+const teams = [{
+        "id": "team-venus",
+        "name": "Team Venus",
+        "code": "AC3",
+        "score": 450
+    },
+    {
+        "id": "team-mars",
+        "name": "Team Mars",
+        "code": "BU8",
+        "score": 420
+    },
+    {
+        "id": "team-mercury",
+        "name": "Team Mercury",
+        "code": "RS3",
+        "score": 400
+    }
+];
+
+router.get('/', (req, res, next) => {
+    res.render('galaxygames/updatestats', { teams });
+});
+
+module.exports = router;
