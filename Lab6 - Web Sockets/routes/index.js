b5a83555cc23430b4f35485496bf48eff32802fb
Bob Storms <bob.storms@hotmail.be>
2021-03-24 20:08:49 +0100

    feat: Add index view for dashboard

--- Lab6 - Web Sockets/routes/index.js
+++ Lab6 - Web Sockets/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const teams = [
+  {
+    "id": "team-venus",
+    "name": "Team Venus",
+    "code": "AC3",
+    "score": 450
+  },
+  {
+    "id": "team-mars",
+    "name": "Team Mars",
+    "code": "BU8",
+    "score": 420
+  },
+  {
+    "id": "team-mercury",
+    "name": "Team Mercury",
+    "code": "RS3",
+    "score": 400
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('galaxygames/index', { teams });
 });
 
 module.exports = router;
