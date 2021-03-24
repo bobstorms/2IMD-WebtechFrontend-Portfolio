dd77d0b88aa085918fe403af8db93025dd59d428
Bob Storms <bob.storms@hotmail.be>
2021-03-24 20:38:54 +0100

    frefactor: add separate folder for mockup data

---
+++ Lab6 - Web Sockets/data/teams.js
@@ -0,0 +1,21 @@
+const teams = [{
+        "id": "team-venus",
+        "name": "Team Venus",
+        "code": "AC3",
+        "score": 450
+    },
+    {
+        "id": "team-mars",
+        "name": "Team Mars",
+        "code": "BU8",
+        "score": 420
+    },
+    {
+        "id": "team-mercury",
+        "name": "Team Mercury",
+        "code": "RS3",
+        "score": 400
+    }
+];
+
+module.exports.teams = teams;
--- Lab6 - Web Sockets/routes/index.js
+++ Lab6 - Web Sockets/routes/index.js
@@ -1,26 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const teams = [
-  {
-    "id": "team-venus",
-    "name": "Team Venus",
-    "code": "AC3",
-    "score": 450
-  },
-  {
-    "id": "team-mars",
-    "name": "Team Mars",
-    "code": "BU8",
-    "score": 420
-  },
-  {
-    "id": "team-mercury",
-    "name": "Team Mercury",
-    "code": "RS3",
-    "score": 400
-  }
-];
+const teams = require("../data/teams").teams;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
